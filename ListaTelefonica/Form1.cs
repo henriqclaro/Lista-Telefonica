5f4f53f3c02e0028af635d51479e6375fda79e24
henriqclaro <henrique.claro@unesp.br>
2025-07-31 12:13:52 -0300

    POO

--- ListaTelefonica/Form1.cs
+++ ListaTelefonica/Form1.cs
@@ -10,58 +10,31 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using ListaTelefonica.Models;
 
 namespace ListaTelefonica
 {
 
     public partial class Form1 : Form
     {
-        string[][] lista;
-        readonly int MAX = 100;
+        List<Contato> lista;
         string selectedId;
         public Form1()
         {
             InitializeComponent();
-            lista = new string[MAX][];
-        }
-
-        int Length(string[][] e)
-        {
-            int itens = 0;
-            for (int i = 0; i < e.Length; i++)
-            {
-                if (e[i] != null)
-                {
-                    itens++;
-                }
-            }
-            return itens;
-        }
-
-        int Length(string[] e)
-        {
-            int itens = 0;
-            for (int i = 0; i < e.Length; i++)
-            {
-                if (e[i] != null)
-                {
-                    itens++;
-                }
-            }
-            return itens;
+            lista = new List<Contato>();
         }
 
         void Atualizar()
         {
             dgvLista.Rows.Clear();
-            for (int i = 0; i < Length(lista); i++)
+            for (int i = 0; i < lista.Count; i++)
             {
                 DataGridViewRow row = new DataGridViewRow();
                 row.CreateCells(dgvLista);
-                for (int j = 0; j < Length(lista[i]); j++)
-                {
-                    row.Cells[j].Value = lista[i][j];
-                }
+                row.Cells[0].Value = lista[i].Id;
+                row.Cells[1].Value = lista[i].Nome;
+                row.Cells[2].Value = lista[i].Telefone;
                 dgvLista.Rows.Add(row);
             }
         }
@@ -74,29 +47,28 @@ namespace ListaTelefonica
                 return;
             }
 
-            if (Length(lista) == MAX)
-            {
-                MessageBox.Show("Lista cheia.");
-                return;
-            }
-            
             if (!String.IsNullOrEmpty(selectedId))
             {
                 int indice = 0;
-                for (indice = 0; indice < Length(lista) && lista[indice][0] != selectedId; indice++);
-                lista[indice][1] = txtNome.Text;
-                lista[indice][2] = txtTel.Text;
+                for (indice = 0; indice < lista.Count && lista[indice].Id+"" != selectedId; indice++);
+                lista[indice].Nome = txtNome.Text;
+                lista[indice].Telefone = txtTel.Text;
             }
 
             else
             {
                 int id = 1;
-                if (Length(lista) > 0)
+                if (lista.Count > 0)
                 {
-                    id = int.Parse(lista[Length(lista) - 1][0]) + 1;
+                    id = lista.Max(c => c.Id) + 1;
                 }
 
-                lista[Length(lista)] = new string[] { id.ToString(), txtNome.Text, txtTel.Text };
+                Contato novo = new Contato();
+                novo.Id = id;
+                novo.Nome = txtNome.Text;
+                novo.Telefone = txtTel.Text;
+
+                lista.Add(novo);
             }
 
             Atualizar();
@@ -115,17 +87,12 @@ namespace ListaTelefonica
             int linha = cell.RowIndex;
             selectedId = dgvLista.Rows[linha].Cells[0].Value.ToString();
 
-            int indice = 0;
-            for (indice = 0; indice < Length(lista) && lista[indice][0] != selectedId; indice++);
+            int indice = lista.FindIndex(c => c.Id+"" == selectedId);
 
-            DialogResult r = MessageBox.Show($"Deseja remover {lista[indice][1]}?", "Remover", MessageBoxButtons.YesNo);
+            DialogResult r = MessageBox.Show($"Deseja remover {lista[indice].Nome}?", "Remover", MessageBoxButtons.YesNo);
             if (r == DialogResult.Yes)
             {
-                for (int i = indice; i <= Length(lista) - 1; i++)
-                {
-                    lista[i] = lista[i + 1];
-                }
-                lista[Length(lista)] = null;
+                lista.RemoveAt(indice);
 
                 Limpar();
                 Atualizar();
---
+++ ListaTelefonica/Models/Contato.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaTelefonica.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}
